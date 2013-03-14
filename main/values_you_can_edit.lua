4de62f6ce0ba07d58f36c20229ccc517d1bbafa9
Josh <uberbreaker@gmail.com>
2013-03-13 21:36:58 -0700

    Added notes for Joe

--- main/values_you_can_edit.lua
+++ main/values_you_can_edit.lua
@@ -1,22 +1,19 @@
 module(..., package.seeall);
 
---Set Screen Size
+--Set Screen/Resolution Size
 resolutionWidth = 1024
 resolutionHeight = 768
---Name of SpriteSheet
+--Name of SpriteSheet (to be placed in the same directory)
 tilesheetlocation = "smurf_sprite.png"
 
---How many images are on each column of the tilesheet
+--How many images are on each column of the main tilesheet
 tilesheet_rows_across = 4
-
---How many columns exist in the tilesheet
+--How many columns exist in the main tilesheet
 tilesheet_columns = 4
-
-
-
---Set how many frames are in the run animation
+--Set how many frames are in the main run animation
 animation_keyframes = 17
-idle_animation_keyframes = 3
+--Set how many frames are in the idle animation
+idle_animation_keyframes = 3			
 
 
 --Set the animation order and time between frames
@@ -41,57 +38,12 @@ curvekeys = {
           { 17, 0.80, 4, 'MOAIEaseType.FLAT' },
 			}
 
+--Set the animation order and time between frames on the idle animation
+--Structure (Order # In Animation, Time Delay, Frame to Pull on the Sheet, MOAItransitionStyle)
+
 idle_curvekeys = {
   { 1, 0.00, 4, 'MOAIEaseType.FLAT' },
   { 2, 1.00, 5, 'MOAIEaseType.FLAT' },
   { 3, 2.00, 6, 'MOAIEaseType.FLAT' },
 
 }
-
-
-
-
-
-
-
-
---[[
-
-
-
-
-module(..., package.seeall);
-
---Set Screen Size
-resolutionWidth = 1024
-resolutionHeight = 768
---Name of SpriteSheet
-tilesheetlocation = "Character.png"
-
---How many images are on each column of the tilesheet
-tilesheet_rows_across = 5
-
---How many columns exist in the tilesheet
-tilesheet_columns = 2
-
-
-
---Set how many frames are in the run animation
-animation_keyframes = 11
-
-
---Set the animation order and time between frames
---Structure (Order # In Animation, Time Delay, Frame to Pull on the Sheet, MOAItransitionStyle)
-curvekeys = {
-          { 1, 0.00, 1, 'MOAIEaseType.FLAT' },
-          { 2, 0.05, 2, 'MOAIEaseType.FLAT' },
-          { 3, 0.10, 3, 'MOAIEaseType.FLAT' },
-          { 4, 0.15, 4, 'MOAIEaseType.FLAT' },
-          { 5, 0.20, 5, 'MOAIEaseType.FLAT' },
-          { 6, 0.25, 6, 'MOAIEaseType.FLAT' },
-          { 7, 0.30, 7, 'MOAIEaseType.FLAT' },
-          { 8, 0.35, 8, 'MOAIEaseType.FLAT' },
-          { 9, 0.40, 9, 'MOAIEaseType.FLAT' },
-          { 10, 0.45, 10, 'MOAIEaseType.FLAT' },
-          { 11, 0.50, 1, 'MOAIEaseType.FLAT' },
-      }]]
